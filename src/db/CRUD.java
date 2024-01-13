98c13ed2e749b0a81cf18315e6510a4176543e8a
Zinnurbek Ahrorov <a.zinnurbek@gmail.com>
2024-01-13 22:44:25 +0500

    Vazifalar tayorlandi, Ishni boshlash kerak, Shu nuqtadan, Update

--- src/db/CRUD.java
+++ src/db/CRUD.java
@@ -3,7 +3,7 @@ package db;
 import bean.BaseIdBean;
 
 public interface CRUD<T extends BaseIdBean> {
-    Integer add(T bean);
+    T add(T bean);
     T get(Integer id);
     Boolean update(T newBean);
     Boolean delete(Integer id);
