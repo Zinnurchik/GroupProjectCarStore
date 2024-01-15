6d4de1348a2a4a41a21f6166d46aa4e46d7964f1
SALIM TUKHTATOSHOV <63718957+SalimBobo@users.noreply.github.com>
2024-01-15 07:38:23 +0500

    Update UserRepository.java

--- src/db/UserRepository.java
+++ src/db/UserRepository.java
@@ -1,5 +1,31 @@
 package db;
 
+import bean.UserBean;
+
+import java.util.List;
+
 public class UserRepository {
-    //TODO resourcega kerak bo'ladigan functionlarni yozish, DB bilan ishlash SalimBobo!!!
+    public static UserBean addUser(UserBean user) {
+        return DB.addUser(user);
+    }
+
+    public static UserBean getUser(int userId) {
+        return DB.getUser(userId);
+    }
+
+    public static boolean updateUser(UserBean newUser) {
+        return DB.setUser(newUser);
+    }
+
+    public static boolean deleteUser(int userId) {
+        return DB.deleteUser(userId);
+    }
+
+    public static UserBean loginUser(UserBean user) {
+        return DB.getUser(user.getLogin(), user.getPassword());
+    }
+
+    public static List<UserBean> getAllUsers() {
+        return DB.getUserList();
+    }
 }
